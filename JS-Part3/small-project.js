03b4744194c1e8698ebd74fb8400665e8f1fa290
akash <rohanbhattacharya173@gmail.com>
2026-04-30 19:36:49 +0530

    created a small project of adding, rendering, users inside cards to understand "this" keyword deeply.

---
+++ JS-Part3/small-project.js
@@ -0,0 +1,50 @@
+let form = document.querySelector("form")
+let username = document.querySelector("#name")
+let bio = document.querySelector("#bio")
+let role = document.querySelector("#role")
+let img = document.querySelector("#photo")
+
+const userManager = {
+    users: [],
+    init: function () {
+        form.addEventListener("submit", this.submitForm.bind(this)) // this -> object
+    },
+    submitForm: function (e) {
+        e.preventDefault();
+        this.addUser();
+        this.renderUi();
+        this.removeUser()
+    },
+    addUser: function () {
+        this.users.push({
+            username: username.value,
+            bio: bio.value,
+            role: role.value,
+            img: img.value
+        })
+        form.reset()
+    },
+    removeUser: function (index) {
+        this.users.splice(index, 1);
+    },
+    renderUi: function () {
+        const container = document.querySelector("#cardContainer");
+        container.innerHTML = "";
+
+        this.users.forEach((user, index) => {
+            const card = document.createElement("div");
+            card.className = "card";
+
+            card.innerHTML = `
+            <img src="${user.img}" />
+            <h3>${user.username}</h3>
+            <p class="role">${user.role}</p>
+            <p class="bio">${user.bio}</p>
+            <button onclick="userManager.removeUser(${index})">Delete</button>
+        `;
+
+            container.appendChild(card);
+        });
+    },
+}
+userManager.init()
